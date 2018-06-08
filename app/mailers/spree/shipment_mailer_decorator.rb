7bbcf54fab2557b19a978db8c46bfca677b1a5c7
sfqi <stvfilipovic@gmail.com>
2018-06-08 11:10:10 +0200

    Fix order & shipment email subject translations

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -5,16 +5,15 @@ Spree::ShipmentMailer.class_eval do
   def shipped_email(shipment, resend = false)
     @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    if @shipment.shipping_method.name == "Osebni prevzem"
-      subject_part = Spree.t('shipment_mailer.shipped_email.osebni_prevzem_subject')
-    else
-      subject_part = Spree.t('shipment_mailer.shipped_email.subject')
-    end
-
     if @shipment.order.ship_address.country.name != "Slovenija"
       @translation = YAML.load_file('config/shipping-eu.yml')
     else
       @translation = YAML.load_file('config/shipping-slo.yml')
+    end
+    if @shipment.shipping_method.name == "Osebni prevzem"
+      subject_part = Spree.t('shipment_mailer.shipped_email.osebni_prevzem_subject')
+    else
+      subject_part = @translation['map']['shipment_mailer']['shipped_email']['subject']
     end
     subject += "#{Spree::Config[:site_name]} #{subject_part} ##{@shipment.order.number}"
     mail(to: @shipment.order.email, from: from_address, subject: subject)
